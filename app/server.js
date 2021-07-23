f7b92289e5ed6248a30f6c2fdf433fa7f4959676
TECHVN\datnm <datnm@mti-tech.vn>
2021-07-23 14:53:12 +0700

    no message

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world from automatic deploment version 1\n');
+  res.send('Hello world from automatic deploment version 2\n');
 });
 
 app.listen(PORT, HOST);
